76c932bd81dda0401fecf827e840391abdf08a53
Viktor Stenqvist <yottster@gmail.com>
2026-02-02 23:06:22 +0100

    write to buffer

---
+++ cmd/wasm/color.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+type Color struct {
+    R, G, B, A uint8
+}
+
+func (c Color) String() string {
+    // TODO: A needs to be normalized
+    return fmt.Sprintf("rgba(%d, %d, %d, %f)",
+        c.R, c.G, c.B, float64(c.A)/255.0)
+}
+func (c Color) Solid() Color {
+    return Color {
+        R: c.R,
+        G: c.G,
+        B: c.B,
+        A: 0xFF,
+    }
+}
+func (c *Color) Packed() uint32 {
+    return uint32(c.R) |
+        uint32(c.G) << 8 |
+        uint32(c.B) << 16 |
+        uint32(c.A) << 24
+}
--- cmd/wasm/main.go
+++ cmd/wasm/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"syscall/js"
+	"strconv"
+	"unsafe"
 )
 
 var privateUniverse *Universe
@@ -15,29 +17,31 @@ func InitGame(this js.Value, args []js.Value) interface{} {
 	
 	return nil
 }
-func Paint(ctx js.Value, 
-	universe *Universe, 
-	cellSize int,
-	status string) {
-	
-	ctx.Call("clearRect", 0, 0, 
-		universe.width * cellSize, 
-		universe.height * cellSize)
 
-	ctx.Set("fillStyle", "red")
-	ctx.Call("fillText", status, 10, 30)
-	
-	ctx.Set("fillStyle", "#000000")
+func Paint(
+	target js.Value,
+	writeCtx js.Value,
+	pixels []byte) {
 
-	for y := 0; y < universe.height; y++ {
-		for x := 0; x < universe.width; x++ {
-			if universe.Alive(x, y) {
-				ctx.Call("fillRect", 
-					x * cellSize, 
-					y * cellSize, 
-					cellSize, 
-					cellSize)
-			}
+	js.CopyBytesToJS(target.Get("data"), pixels)
+	writeCtx.Call("putImageData", target, 0, 0)
+}
+func TransferImage(
+	srcCanvas js.Value,
+	ctx js.Value,
+	width int,
+	height int) {
+
+	ctx.Call("drawImage", srcCanvas, 0, 0, width, height)
+}
+
+func Render(u *Universe, dst []uint32, d uint32, l uint32) {
+	
+	for i, cell := range u.cells {
+		if cell == 1 {
+			dst[i] = l
+		} else {
+			dst[i] = d
 		}
 	}
 }
@@ -46,12 +50,42 @@ func PrintStatus(ctx js.Value, status string) {
 	ctx.Set("fillStyle", "red")
 	ctx.Call("fillText", status, 10, 30)
 }
+
 func main() {
 	window := js.Global()
 	document := window.Get("document")
+
+	body := document.Get("body")
+	bodyStyle := body.Get("style")
+
+	deadColor := Color{0,0,0,200}
+	liveColor := Color{0,0,0xFF,0xFF}
+
+	deadPacked := deadColor.Packed()
+	livePacked := liveColor.Packed()
+
+	bodyStyle.Set("background", deadColor.Solid().String())
+	bodyStyle.Set("margin", "0")
+	bodyStyle.Set("padding", "0")
+	
 	canvas := document.Call("getElementById", "canvas")
+	canvasStyle := canvas.Get("style")
+	canvasStyle.Set("display", "block")
+	canvasStyle.Set("overflow", "hidden")
+	
 	ctx := canvas.Call("getContext", "2d")
-	cellSize := 3
+
+	location := window.Get("location")
+	search := location.Get("search").String()
+	params := window.Get("URLSearchParams").New(search)
+	sizeParam := params.Call("get", "cellSize")
+	var cellSize int = 3
+	if !sizeParam.IsNull() {
+		val, err := strconv.Atoi(sizeParam.String())
+		if err == nil {
+			cellSize = val
+		}
+	}
 	
 	innerWidth := window.Get("innerWidth").Int()
 	innerHeight := window.Get("innerHeight").Int()
@@ -61,15 +95,30 @@ func main() {
 
 	width := innerWidth/cellSize
 	height := innerHeight/cellSize
+
+	hiddenCanvas := document.Call(
+		"createElement", "canvas")
+	hiddenCanvas.Set("width", width)
+	hiddenCanvas.Set("height", height)
+	writeCtx := hiddenCanvas.Call("getContext", "2d")
 	
-	currentUniverse := NewUniverse(width, height)
+	pixels := make([]byte, 4 * width * height)
+	ptr := unsafe.Pointer(&pixels[0])
+	u32Buffer := 
+		unsafe.Slice((*uint32)(ptr), width * height)
+	
+	imageData := writeCtx.Call(
+		"createImageData", width, height)
+
 	nextUniverse := NewUniverse(width, height)
-	
+	currentUniverse := NewUniverse(width, height)
 	currentUniverse.seed()
 
 	performance := window.Get("performance")
 	frameTimer := NewFrameTimer(performance, 30)
+	
 	ctx.Set("font", "40px monospace")
+	ctx.Set("imageSmoothingEnabled", false)
 	var renderFrame js.Func
 	var status string
 	
@@ -77,8 +126,12 @@ func main() {
 
 		start := frameTimer.Start()
 
-		Paint(ctx, currentUniverse, cellSize, status)
-		
+		Render(currentUniverse, u32Buffer, deadPacked, livePacked)
+
+		Paint(imageData, writeCtx, pixels)
+		TransferImage(hiddenCanvas, ctx, innerWidth, innerHeight)
+		PrintStatus(ctx, status)
+
 		currentUniverse.Next(nextUniverse)
 		currentUniverse, nextUniverse = nextUniverse, currentUniverse
 
